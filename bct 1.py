582c0779bc03f94ddfce85f8aa11e7c0aedffcc9
Guruvardhana M <162596167+guruvardhan-tech-village@users.noreply.github.com>
2025-11-20 07:12:42 +0530

    Delete bct 1.py

--- bct 1.py
+++
@@ -1,7 +0,0 @@
-openssl genpkey -algorithm RSA -out private_key.pem pkeyopt 
-rsa_keygen_bits:2048   
-
-cat private_key.pem   
-  openssl rsa -pubout -in private_key.pem -out public_key.pem  
-cat public_key.pem 
-
